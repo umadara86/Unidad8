61c59d3a75999e8a05a25c3befce389ee9cfaba7
Angel Soldado Jorge <ansoldado@AnSoldado-Mac.local>
2016-11-13 12:25:45 +0100

    Añadida solicitud de permisos (Hay que probarlo)

--- app/src/main/java/com/example/ivan/unidad8/MapsActivity.java
+++ app/src/main/java/com/example/ivan/unidad8/MapsActivity.java
@@ -12,6 +12,7 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -31,6 +32,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     //EJERCICIO REDES
     private static final long TIEMPO_MIN = 10 * 1000; // 10 segundos
     private static final long DISTANCIA_MIN = 5; // 5 metros
+    private static final int SOLICITUD_PERMISO_ACCURACY_FINE = 0;
 
     private LocationManager manejador;
     private String proveedor;
@@ -41,30 +43,37 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Obtenemos el mapa de forma asíncrona (notificará cuando esté listo)
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapa);
-        mapFragment.getMapAsync(this);
+        iniciarMapa();
+    }
 
-        //EJERCICIO REDES
-        manejador = (LocationManager) getSystemService(LOCATION_SERVICE);
+    private void iniciarMapa(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            // Obtenemos el mapa de forma asíncrona (notificará cuando esté listo)
+            SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapa);
+            mapFragment.getMapAsync(this);
 
-        Criteria criterio = new Criteria();
-        criterio.setCostAllowed(false);
-        criterio.setAltitudeRequired(false);
-        criterio.setAccuracy(Criteria.ACCURACY_FINE);
-        proveedor = manejador.getBestProvider(criterio, true);
+            //EJERCICIO REDES
+            manejador = (LocationManager) getSystemService(LOCATION_SERVICE);
 
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
+            Criteria criterio = new Criteria();
+            criterio.setCostAllowed(false);
+            criterio.setAltitudeRequired(false);
+            criterio.setAccuracy(Criteria.ACCURACY_FINE);
+            proveedor = manejador.getBestProvider(criterio, true);
+
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+        } else {
+            solicitarPermisoLocaclizacion();
         }
-
     }
 
     @Override
@@ -154,7 +163,30 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
     }
 
+    void solicitarPermisoLocaclizacion() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                ActivityCompat.requestPermissions(this, new String[]{
+                        Manifest.permission. ACCESS_FINE_LOCATION },SOLICITUD_PERMISO_ACCURACY_FINE);
+        }else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, SOLICITUD_PERMISO_ACCURACY_FINE);
+        }
+    }
 
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == SOLICITUD_PERMISO_ACCURACY_FINE) {
+            if (grantResults.length== 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast toast2 = Toast.makeText(getApplicationContext(), "Permisos de posicionamiento concedidos.", Toast.LENGTH_LONG);
+                toast2.show();
+                iniciarMapa();
+            } else {
+                Toast toast2 = Toast.makeText(getApplicationContext(), "Permisos de posicionamiento denagados.", Toast.LENGTH_LONG);
+                toast2.show();
+            }
+        }
+    }
 
 }
 
